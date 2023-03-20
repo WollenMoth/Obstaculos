f38e1d2a89cc4fcae774b5c60e4626ab834341e9
André Díaz <andre290800@gmail.com>
2023-03-20 12:28:13 -0600

    style: 🎨 rename variable

--- models/player.py
+++ models/player.py
@@ -11,9 +11,9 @@ SPRITE_SIZE = (32, 32)
 class Player(Animated):
     """Representa al jugador"""
 
-    def __init__(self, start: Coordinate) -> None:
+    def __init__(self, center: Coordinate) -> None:
         """Inicializa el jugador"""
-        super().__init__(pygame.Rect(start, PLAYER_SIZE), "frog", SPRITE_SIZE, True)
+        super().__init__(pygame.Rect(center, PLAYER_SIZE), "frog", SPRITE_SIZE, True)
 
         self.velocity = PLAYER_VELOCITY
 
